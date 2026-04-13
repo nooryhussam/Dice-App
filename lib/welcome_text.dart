86b1297857809f60fe05b2af3c1af31aa5d94f8e
nooryhussam <noorhussam494@gmail.com>
2026-04-13 13:49:20 +0200

    Add files

---
+++ lib/welcome_text.dart
@@ -0,0 +1,16 @@
+// import 'package:flutter/material.dart';
+
+// class WelcomeText extends StatelessWidget {
+//   const WelcomeText({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Text(
+//       'Welocme to My App,Please Enjoy it ',
+//       style: TextStyle(
+//         color: const Color.fromARGB(255, 236, 234, 233),
+//         fontSize: 28,
+//       ),
+//     );
+//   }
+// }
